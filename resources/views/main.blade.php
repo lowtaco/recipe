131e69d4c472f8aacd9304bb060c76c65a580c0a
Paintedfriend <ontimeyarik@gmail.com>
2023-09-24 12:24:43 +0400

    Dev

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Food</title>
+        <title>Recipe</title>
 
         <link href="{{ mix('css/app.css') }}" rel="stylesheet">
     </head>
